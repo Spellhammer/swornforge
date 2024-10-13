df646f9be4e60bc34d0124fbd4ebf8cb455e4c5d
Elijah Mills <elijahmills@hey.com>
2024-10-13 08:51:29 -0400

    More progress.

--- engine.js
+++ engine.js
@@ -22,26 +22,70 @@ let swornForgeAssetCreator = function() {
         },
         powers: [
             {
-                title: "Eager",
+                title: "Eager:",
                 unlocked: false,
                 description: "When your cat chases down big game, you may Resupply with +edge (instead of +wits). If you do, take +1 supply or +1 momentum on a strong hit."
             },
             {
-                title: "Inescapable",
+                title: "Inescapable:",
                 unlocked: false,
                 description: "When you Enter the Fray or Strike by sending your cat to attack, roll +edge. On a hit, take +2 momentum."
             },
             {
-                title: "Protective",
+                title: "Protective:",
                 unlocked: false,
                 description: "When you Make Camp, your cat is alert to trouble. If you or an ally choose to relax, take +1 spirit. If you focus, take +1 momentum."
             }
         ],
         moves: [
+            "Face Danger",
+            "Secure an Advantage",
+            "Gather Information",
+            "Heal",
             "Resupply",
+            "Make Camp",
+            "Undertake a Journey",
+            "Reach Your Destination",
+            "Compel",
+            "Sojourn",
+            "Draw the Circle",
+            "Forge a Bond",
+            "Test Your Bond",
+            "Aid Your Ally",
+            "Write Your Epilogue",
             "Enter the Fray",
             "Strike",
-            "Make Camp"
+            "Clash",
+            "Turn the Tide",
+            "End the Fight",
+            "Battle",
+            "Endure Harm",
+            "Face Death",
+            "Companion Endure Harm",
+            "Endure Stress",
+            "Face Desolation",
+            "Out of Supply",
+            "Face a Setback",
+            "Swear an Iron Vow",
+            "Reach a Milestone",
+            "Fulfill Your Vow",
+            "Forsake Your Vow",
+            "Advance",
+            "Pay the Price",
+            "Ask the Oracle",
+            "Discover a Site",
+            "Delve the Depths",
+            "Find an Opportunity",
+            "Reveal a Danger",
+            "Check Your Gear",
+            "Locate Your Objective",
+            "Escape the Depths",
+            "Reveal a Danger",
+            "Mark Your Failure",
+            "Learn From Your Failures",
+            "Advance a Threat",
+            "Take a Hiatus",
+            "Wield a Rarity"
         ],
         transformers: {
             italicizeMoveNames: function( string, moves ) {
@@ -51,6 +95,17 @@ let swornForgeAssetCreator = function() {
                 } );
 
                 return string;
+            }
+        },
+        methods: {
+            download: function( node ) {
+                let link = document.getElementById('download-link');
+                let output = document.getElementById('output');
+
+                html2canvas(node).then(canvas => {
+                    output.innerHTML = '';
+                    output.appendChild(canvas);
+                });
             }
         }
     }
